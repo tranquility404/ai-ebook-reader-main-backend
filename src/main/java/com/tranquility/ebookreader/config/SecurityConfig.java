309a35653438759f18243bd43ef0586b564ffa2e
MyselfRebel <56184411+MyselfRebel@users.noreply.github.com>
2025-01-16 01:56:10 +0530

    ...

--- src/main/java/com/tranquility/ebookreader/config/SecurityConfig.java
+++ src/main/java/com/tranquility/ebookreader/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://yourdomain.com"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://ai-ebook-reader.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
